2c4e2193c72e42f86d5654f57069bc62892e8f6c
Octavio Resendiz Bolaños <octavioresendiz36@aragon.unam.mx>
2022-04-14 15:17:50 -0500

    Agregue las imagenes principales

--- app/src/main/java/com/example/zaguan/Menu.kt
+++ app/src/main/java/com/example/zaguan/Menu.kt
@@ -2,11 +2,43 @@ package com.example.zaguan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val ivQuesadillas = findViewById<ImageView>(R.id.IVQuesadillas)
+        val ivTostadas = findViewById<ImageView>(R.id.IVTostadas)
+        val ivPambazos = findViewById<ImageView>(R.id.IVPambazos)
+        val ivPozole = findViewById<ImageView>(R.id.IVPozole)
+        val ivPostres = findViewById<ImageView>(R.id.IVPostres)
+        val ivBebidas = findViewById<ImageView>(R.id.IVBebidas)
+
+        ivQuesadillas.setOnClickListener {
+
+        }
+
+        ivTostadas.setOnClickListener {
+
+        }
+
+        ivPambazos.setOnClickListener {
+
+        }
+
+        ivPozole.setOnClickListener {
+
+        }
+
+        ivPostres.setOnClickListener {
+
+        }
+
+        ivBebidas.setOnClickListener {
+
+        }
 
 
     }
