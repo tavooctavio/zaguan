787e352124de745db48430a6164b1e324c7f478f
Octavio Resendiz Bolaños <octavioresendiz36@aragon.unam.mx>
2022-04-14 15:31:00 -0500

    Agregue el intent para cada platillo

--- app/src/main/java/com/example/zaguan/Menu.kt
+++ app/src/main/java/com/example/zaguan/Menu.kt
@@ -1,5 +1,6 @@
 package com.example.zaguan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -17,27 +18,33 @@ class Menu : AppCompatActivity() {
         val ivBebidas = findViewById<ImageView>(R.id.IVBebidas)
 
         ivQuesadillas.setOnClickListener {
-
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
         ivTostadas.setOnClickListener {
-
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
         ivPambazos.setOnClickListener {
-
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
         ivPozole.setOnClickListener {
-
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
         ivPostres.setOnClickListener {
-
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
         ivBebidas.setOnClickListener {
-
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
 
